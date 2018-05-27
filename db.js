18954af548d4ec027e3ebc0d6e668b780ed549d4
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-05-27 21:09:27 +0530

    Add db
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

---
+++ db.js
@@ -0,0 +1,51 @@
+const Sequelize = require('sequelize')
+
+const db = new Sequelize('shopdb', 'shopper', 'shoppass', {
+  host: 'localhost',
+  dialect: 'postgres',
+  pool: {
+    min: 0,
+    max: 5,
+  }
+})
+
+const User = db.define('users', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  }
+})
+
+const Product = db.define('products', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  manufacturer: Sequelize.STRING,
+  price: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+    defaultValue: 0.0
+  }
+})
+
+const Cart = db.define('carts', {
+
+})
+db.sync()
+  .then(() => console.log("Database has been synced"))
+  .catch((err) => console.error("Error creating database"))
+
+exports = module.exports = {
+  User, Product
+}
