7be4a4596dde51c6e97b05b06cccbf04be723ead
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-05-29 00:23:12 +0530

    Update products route
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,38 +1,63 @@
 const Product = require('../../db').Product
+const Cart = require('../../db').Cart
+const User = require('../../db').User
 const route = require('express').Router();
 
 route.get('/', (req, res) => {
-    // Get all products
-    Product.findAll()
-        .then((products) => {
-            res.status(200).send(products)
+  // Get all products
+  Product.findAll({
+    include: [
+      {
+        model: Cart,
+        include: [{
+          model: User,
+          where: {
+            name: req.query.name,
+            password: req.query.password
+          }
+        }]
+      }
+    ]
+  }).then((products) => {
+    products = products.map((product) => {
+      product = product.get()
+      if (product.carts) {
+        product.carts = product.carts.map((cart) => {
+          cart = cart.get();
+          cart.user = cart.user.get();
+          return cart;
         })
-        .catch((err) => {
-            res.status(500).send({
-                error: "Could not retrieve products"
-            })
-        })
+      }
+      return product;
+    })
+    res.status(200).send(products)
+  }).catch((err) => {
+    console.log(err)
+    res.status(500).send({
+      error: "Could not retrieve products"
+    })
+  })
 })
 
 route.post('/', (req, res) => {
-    // Validate the values
-    if (isNaN(req.body.price)) {
-        return res.status(403).send({
-            error: "Price is not valid number"
-        })
-    }
-    // Add a new product
-    Product.create({
-        name: req.body.name,
-        manufacturer: req.body.manufacturer,
-        price: parseFloat(req.body.price)
-    }).then((product) => {
-        res.status(201).send(product)
-    }).catch((error) => {
-        res.status(501).send({
-            error: "Error adding product"
-        })
+  // Validate the values
+  if (isNaN(req.body.price)) {
+    return res.status(403).send({
+      error: "Price is not valid number"
     })
+  }
+  // Add a new product
+  Product.create({
+    name: req.body.name,
+    manufacturer: req.body.manufacturer,
+    price: parseFloat(req.body.price)
+  }).then((product) => {
+    res.status(201).send(product)
+  }).catch((error) => {
+    res.status(501).send({
+      error: "Error adding product"
+    })
+  })
 })
 
 exports = module.exports = route
